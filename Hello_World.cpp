1443660dd7b03ab11b13e90d082b08869a6f37e9
ConAntares <LukeNiu@outlook.com>
2019-05-02 13:03:10 +0800

    Update

---
+++ Hello_World.cpp
@@ -0,0 +1,10 @@
+/* Everything Starts Here */
+
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Hello, World!";        // message
+    cout << endl;                   // start a new line
+    return 0;                       // terminate main()
+}
